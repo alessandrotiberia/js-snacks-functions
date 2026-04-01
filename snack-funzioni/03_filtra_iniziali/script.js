e9adaba7d1397a8d0f0466b56eb698d9ef8d4682
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-01 17:13:19 +0200

    esercizio 3 completato

--- snack-funzioni/03_filtra_iniziali/script.js
+++ snack-funzioni/03_filtra_iniziali/script.js
@@ -1,13 +1,22 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera 
+e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
-
+function filtra_per_iniziale(array, lettera) {
+  // Usiamo il metodo filter per creare un nuovo array con gli elementi corretti
+  return array.filter(parola => parola.startsWith(lettera));
+  
+  // function(parola) { questo è c
+  // return parola.startsWith(lettera);
+  //  } 
+    
+}
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const risultato = filtra_per_iniziale(names, "A");
+console.log(risultato);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
