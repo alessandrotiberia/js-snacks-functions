c6470f9f71cd30183db58d829bb88e79ab430c83
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-01 15:13:00 +0200

    esercizio 01 completato

--- snack-funzioni/01_saluti/script.js
+++ snack-funzioni/01_saluti/script.js
@@ -1,12 +1,18 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome e 
+restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
 const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function saluto(userName) {
+    return `Ciao ${userName}`;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(saluto(userName));
+
 
 
 
