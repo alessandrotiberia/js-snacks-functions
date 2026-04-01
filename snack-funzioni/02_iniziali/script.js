432214e12f90194dbe7f11c4a2028ce53042e02f
Alessandro Tiberia <tiberia7030@gmail.com>
2026-04-01 16:26:56 +0200

    esercizio 2 completato

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -1,13 +1,22 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca 
+nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function iniziali(names) {
+    const risultato = []; // dove mettere le iniziali
 
-
+for ( let i = 0; i === names.length; i++ ) {
+    const nome_inserito = names[i]; // scorre ciclo e inserisce dentro variabile
+    risultato.push(nome_inserito[0]); // inserisce dentro risultato
+}
+        return risultato; //restitusce risultato con le iniziali
+}
 // Invoca la funzione qui e stampa il risultato in console
-
+    const array_risultato = iniziali(names);
+    console.log(array_risultato);
+     
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
